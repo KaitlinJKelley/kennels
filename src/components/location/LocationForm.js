840f9a85f850ec2d5dd8518ba90c8de1a0e764c3
kjk1325e <kaitlinjkelley@gmail.comm>
2021-03-01 15:21:11 -0600

    Started NewLocation component

---
+++ src/components/location/LocationForm.js
@@ -0,0 +1,7 @@
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory } from 'react-router-dom';
+import { LocationContext } from "./LocationProvider"
+
+export const LocationForm = () => {
+    const {  }
+}
